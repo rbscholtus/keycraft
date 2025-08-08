e309c785b3460fd0c034f531ea7108552f25fba6
Barend Scholtus <barend.scholtus@gmail.com>
2025-08-09 00:02:54 +0700

    Move processing code to internal

--- cmd/main/rank.go
+++ cmd/main/rank.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
-	"sort"
 	"strconv"
 	"strings"
 
@@ -46,91 +43,7 @@ func rankAction(c *cli.Context) error {
 
 	style := c.String("style")
 
-	return doRankings(corp, layoutDir, weights, style)
-}
-
-// LayoutScore represents a layout's score, including its name and penalty value.
-type LayoutScore struct {
-	Name    string
-	Penalty float64
-}
-
-// doRankings performs the ranking of layouts based on the corpus and weights.
-func doRankings(corpus *ly.Corpus, layoutsDir string, weights map[string]float64, style string) error {
-	fmt.Printf("Ranking layouts in data/layouts with %s and weights: %v\n", corpus, weights)
-
-	// Read layouts
-	layoutFiles, err := os.ReadDir(layoutsDir)
-	if err != nil {
-		return fmt.Errorf("error finding layout files in %v: %v", layoutsDir, err)
-	}
-
-	// Analyze layouts
-	var analysers = make([]*ly.Analyser, 0)
-	var metrics = make(map[string][]float64)
-	for _, file := range layoutFiles {
-		if !strings.HasSuffix(file.Name(), ".klf") {
-			continue
-		}
-		layoutPath := filepath.Join("data", "layouts", file.Name())
-		layout, err := ly.NewLayoutFromFile(file.Name(), layoutPath)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-		analyser := ly.NewAnalyser(layout, corpus, style)
-		analysers = append(analysers, analyser)
-		for metric, value := range analyser.Metrics {
-			metrics[metric] = append(metrics[metric], float64(value))
-		}
-	}
-
-	// Calculate median and interquartile range for each metric
-	// This is used to normalize/scale the metric values
-	medians := make(map[string]float64)
-	iqr := make(map[string]float64)
-	for metric, values := range metrics {
-		sort.Float64s(values)
-		medians[metric] = ly.Median(values)
-		q1, q3 := ly.Quartiles(values)
-		iqr[metric] = q3 - q1
-
-	}
-
-	// Scale and sum metrics
-	// Calculate the penalty for each layout based on the scaled metric values and weights
-	var layoutPenalties []LayoutScore
-	for _, analyser := range analysers {
-		penalty := 0.0
-		for metric, value := range analyser.Metrics {
-			weight, ok := weights[metric]
-			if !ok {
-				// default weight, if unspecified
-				weight = 1.0
-			}
-			var scaledValue float64
-			if iqr[metric] == 0 {
-				scaledValue = 0
-			} else {
-				scaledValue = (value - medians[metric]) / iqr[metric]
-			}
-			penalty += weight * scaledValue
-		}
-		layoutPenalties = append(layoutPenalties, LayoutScore{analyser.Layout.Name, penalty})
-	}
-
-	// Rank layouts
-	sort.Slice(layoutPenalties, func(i, j int) bool {
-		return layoutPenalties[i].Penalty < layoutPenalties[j].Penalty
-	})
-
-	// Print results
-	fmt.Println("Ranking:")
-	for i, layoutPenalty := range layoutPenalties {
-		fmt.Printf("%d. %s (Penalty: %.2f)\n", i+1, layoutPenalty.Name, layoutPenalty.Penalty)
-	}
-
-	return nil
+	return ly.DoRankings(corp, layoutDir, weights, style)
 }
 
 // parseWeights parses a string of weighted metrics into a map of metric names to weights.
